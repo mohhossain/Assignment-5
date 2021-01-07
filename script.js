73ec57e934951f2212bd07b875d546cc6c200a7b
Sam Kessler <sholomkessler1@gmail.com>
2021-01-07 16:44:14 -0500

    Fixed edge case errors

--- script.js
+++ script.js
@@ -2,17 +2,19 @@ let table = document.querySelector('table');
 
 let addRowsButton = document.getElementById('addRows');
 addRowsButton.addEventListener('click', (ev) => {
-	adjustTableDimensions(table.rows[0].cells.length, table.rows.length + 1);
+	adjustTableDimensions(table.rows[0] == undefined ? 0 : table.rows[0].cells.length, table.rows.length + 1);
 	console.log('add row');
 });
 
-let removeRowsButtons = document.getElementById('removeRows');
-removeRowsButtons.addEventListener('click', (ev) => {
-	adjustTableDimensions(table.rows[0].cells.length, table.rows.length - 1);
+let removeRowsButton = document.getElementById('removeRows');
+removeRowsButton.addEventListener('click', (ev) => {
+	adjustTableDimensions(table.rows[0] == undefined ? 0 : table.rows[0].cells.length, table.rows.length - 1);
 	console.log('remove row');
 });
+
 function adjustTableDimensions(newX, newY) {
-	let currentX = table.rows[0].length;
+	if (newY < 0) newY = 0;
+	if (newX < 0) newX = 0;
 	for (let y = 0; y < newY; y++) {
 		let row = table.rows[y];
 		if (row == undefined) {
